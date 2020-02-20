e2b79371322d1de2e5997e36eea46fc8f92b4c38
mcb <17601330731@163.com>
2020-02-20 10:50:49 +0800

    first push

---
+++ src/components/home/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="font">
+      首页
+      <div>
+        首页2
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+ @import "./home.less";
+</style>

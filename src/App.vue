94f0428183c398c1a0f48c788d0458efe6b7ba64
mcb <17601330731@163.com>
2020-02-21 14:28:21 +0800

    2-21 公共组件

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,16 @@ export default {
 
 <style>
   *{
-    margin: 0;
-    padding: 0;
+    margin:0;
+    padding:0;
+  }
+  html,body{
+    width:100%;
+    height:100%;
     font-size: 14px;
     color: #3B4162;
   }
-  ul,li{
-    list-style: none;
-  }
+  ul{list-style:none}
+  a{text-decoration: none;}
+  img{vertical-align:top;border:none}
 </style>

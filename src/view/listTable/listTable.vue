278722ecf518770ba973eaab247ce8d14df54c3f
zhaoxiaohe123 <1435433847@qq.com>
2020-02-24 18:31:48 +0800

    commit

--- src/view/listTable/listTable.vue
+++ src/view/listTable/listTable.vue
@@ -105,6 +105,23 @@
     methods:{
       deleteRow(index, rows) {
         rows.splice(index, 1);
+        // console.log("-----1",MessageBox)
+        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+            // rows.splice(index, 1);
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });          
+        });
       },
       handleClick(row) {
         console.log(row);
